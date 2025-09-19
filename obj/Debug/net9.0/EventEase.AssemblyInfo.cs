c4a940a67b0fc55c17cbe6c1bd66f4cb5aec23f4
Warren Miraflor <warrenmrflr@gmail.com>
2025-09-19 13:16:17 +0800

    Fix routing conflict by removing duplicate Home page

--- obj/Debug/net9.0/EventEase.AssemblyInfo.cs
+++ obj/Debug/net9.0/EventEase.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3105e8f76a6239cedb79f770b0eabdbaf79edb02")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
